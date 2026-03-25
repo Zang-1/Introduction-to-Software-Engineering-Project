2305c6684e447ad71807d77cf056828682d9d8d7
zang <giangbao041@gmail.com>
2026-03-26 06:42:48 +0700

    Change print statement to English for patient list

--- view_db.py
+++ view_db.py
@@ -4,7 +4,7 @@ os.system(
     'start cmd /k "python -c \"import sqlite3; '
     'conn=sqlite3.connect(\'patients.db\'); '
     'cursor=conn.cursor(); '
-    'print(\'=== DANH SÁCH PATIENT ===\\n\'); '
+    'print(\'=== PATIENT LIST ===\\n\'); '
     'rows=cursor.execute(\'SELECT * FROM patients\').fetchall(); '
     '[print(r) for r in rows]; '
     'input(\'\\nPress Enter to exit...\'); '
