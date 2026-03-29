58bf1fa70b07040478bc036bccab046cf8ce6e49
zang <giangbao041@gmail.com>
2026-03-29 20:35:39 +0700

    Delete patients.sql

--- patients.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS patients;
-
-CREATE TABLE IF NOT EXISTS patients (
-    patient_id TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INTEGER,
-    gender TEXT,
-    phone TEXT
-);
